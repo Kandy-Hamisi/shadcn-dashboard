699a8ea0f23962d1162074c9d35b3f354bdca7ba
Kandy-Hamisi <hkandy2000@gmail.com>
2025-05-14 11:01:46 +0300

    added bar, area and doughnut charts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
 import Image from "next/image";
+import AppChart from "@/components/AppChart";
+import AppAreaChart from "@/components/AppAreaChart";
+import AppPieChart from "@/components/AppPieChart";
 
 export default function Home() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
       <div className="bg-primary-foreground rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
-        Test
+        <AppChart />
       </div>
       <div className="bg-primary-foreground rounded-lg">Test</div>
-      <div className="bg-primary-foreground rounded-lg">Test</div>
+      <div className="bg-primary-foreground rounded-lg">
+        <AppPieChart />
+      </div>
       <div className="bg-primary-foreground rounded-lg">Test</div>
       <div className="bg-primary-foreground rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
-        Test
+        <AppAreaChart />
       </div>
       <div className="bg-primary-foreground rounded-lg">Test</div>
       <div className="bg-primary-foreground rounded-lg">Test</div>
